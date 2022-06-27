3c369daf6425284060bd813f3bf16714d2d2e213
efuraimujs <efuraimu.js@gmail.com>
2022-06-27 05:22:39 +0300

    project-setup

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=root
+export POSTGRES_PASSWORD=rootpass
+export POSTGRES_HOST=project3-db-1656284691.cocqdrux33zf.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::project3-bucket-1656284691
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
